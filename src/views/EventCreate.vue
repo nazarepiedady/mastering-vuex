357a3643906f4c25934512cd233c2b7a8ce3c92b
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-02-18 12:58:45 +0100

    Use computed property and mapState object
    To show user and categories data

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,18 +2,16 @@
   <div>
     <h1>Create an Event, {{ user.name }}</h1>
     <p>This event was created by {{ user.name }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  computed: {
-    user() {
-      return {
-        id: this.$store.state.user.id,
-        name: this.$store.state.user.name,
-      }
-    },
-  },
+  computed: mapState(['user', 'categories']),
 }
 </script>

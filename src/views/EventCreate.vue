6915d7d7be79a21446622b4b6f1b1608225c44b0
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-02-18 12:29:50 +0100

    Config EventCreate
    To show user information stored in Vuex store

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.name }}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    user() {
+      return {
+        id: this.$store.state.user.id,
+        name: this.$store.state.user.name,
+      }
+    },
+  },
+}
+</script>
